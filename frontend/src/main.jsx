a15eb2b9729ba23d3230e3463f0394e70fe7d449
Aya Salama <asalama0204@gmail.com>
2024-09-20 18:13:42 -0400

    Imported EmployeePage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import AdminPage from './pages/AdminPage.jsx'
 import ManagerPage from './pages/ManagerPage.jsx'
+import EmployeePage from './pages/EmployeePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
