e60f1531a4ec1d32482412fe7dd8ebc33ec1c016
Jtawfik04 <johntawfik106@gmail.com>
2024-09-20 19:49:48 -0400

    add admin manager

--- Backend/db.py
+++ Backend/db.py
@@ -1,7 +1,7 @@
-# database_driver.py
-
 import sqlite3
-from worker import WorkerManager  # Import WorkerManager
+from schemas import WorkerManager  
+from schemas import ClientManager
+from schemas import AdminManager
 import datetime
 import bcrypt
 import hashlib
@@ -10,6 +10,8 @@ import os
 class DatabaseDriver(object):
     def create_tables(self):
         self.worker_manager.create_worker_table()
+        self.client_manager.create_client_table()
+        self.admin_manager.create_admin_table()
         self.create_client_table()
         self.create_boss_table()
         self.create_location_table()
@@ -19,6 +21,8 @@ class DatabaseDriver(object):
         self.conn = sqlite3.connect("safety.db")
         self.cursor = self.conn.cursor()
         self.worker_manager = WorkerManager(self.conn)
+        self.client_manager = ClientManager(self.conn)
+        self.admin_manager = AdminManager(self.conn)
         self.create_tables()
 
 
