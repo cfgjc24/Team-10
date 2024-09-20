33ee441fa75176122870d0be42d2e931431f0f24
Jtawfik04 <johntawfik106@gmail.com>
2024-09-20 18:53:59 -0400

    abstract workers from db.py

---
+++ Backend/schemas/worker.py
@@ -0,0 +1,115 @@
+# worker.py
+
+import datetime
+
+class WorkerManager:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def create_worker_table(self):
+        """
+        Creates a worker table using SQL
+        """
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS worker(
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT NOT NULL,
+                                manager INTEGER FOREIGN KEY NOT NULL,
+                                status TEXT CHECK(status IN ('away', 'approaching', 'arrived')) NOT NULL, 
+                                last_update TIME null,
+                                check_in TIME null,
+                                check_out TIME null
+                            );""")
+        self.conn.commit()
+
+    def delete_worker_table(self):
+        """
+        Deletes the worker table using SQL
+        """
+        self.conn.execute("""DROP TABLE IF EXISTS worker""")
+        self.conn.commit()
+
+    def get_all_workers(self):
+        """
+        Returns all workers in the table using SQL
+        """
+        cursor = self.conn.execute("""SELECT * FROM worker""")
+        workers = []
+        for row in cursor:
+            workers.append({"id": row[0], "name": row[1], "manager": row[2], "status": row[3], 
+                            "last_update": row[4], "check_in": row[5], "check_out": row[6]})
+        return workers
+
+    def get_worker_by_id(self, worker_id):
+        """
+        Returns a worker by its ID
+        """
+        cursor = self.conn.execute("SELECT * FROM worker WHERE id = ?;", (worker_id,))
+        for row in cursor:
+            return {"id": row[0], "name": row[1], "manager": row[2], "status": row[3],
+                    "last_update": row[4], "check_in": row[5], "check_out": row[6]}
+        return None
+
+    def insert_worker_table(self, name, manager_id):
+        """
+        Inserts a new worker into the worker table with default values.
+        """
+        time_now = datetime.datetime.now()
+        cursor = self.conn.execute("""
+            INSERT INTO worker (name, manager, status, last_update, check_in, check_out)
+            VALUES (?, ?, ?, ?, ?, ?);
+        """, (name, manager_id, 'away', time_now, None, None))
+        
+        self.conn.commit()
+        return cursor.lastrowid
+
+    def delete_worker_from_table(self, worker_id):
+        """
+        Deletes a worker from the worker table using SQL
+        """
+        self.conn.execute("DELETE FROM worker WHERE id = ?;", (worker_id,))
+        self.conn.commit()
+
+    def update_worker_updated_time(self, worker_id):
+        """
+        Updates the last update time of a worker.
+        """
+        curr_time = datetime.datetime.now()
+        self.conn.execute("""
+            UPDATE worker
+            SET last_update = ?
+            WHERE id = ?;
+        """, (curr_time, worker_id))
+        self.conn.commit()
+
+    def update_worker_status(self, worker_id, status):
+        """
+        Updates the status of a worker.
+        """
+        self.conn.execute("""
+            UPDATE worker
+            SET status = ?
+            WHERE id = ?;
+        """, (status, worker_id))
+        self.conn.commit()
+
+    def update_check_in(self, worker_id, check_in_time):
+        """
+        Updates the check-in time of a worker.
+        """
+        self.conn.execute("""
+            UPDATE worker
+            SET check_in = ?
+            WHERE id = ?;
+        """, (check_in_time, worker_id))
+        self.conn.commit()
+
+    def update_check_out(self, worker_id, check_out_time):
+        """
+        Updates the check-out time of a worker.
+        """
+        self.conn.execute("""
+            UPDATE worker
+            SET check_out = ?
+            WHERE id = ?;
+        """, (check_out_time, worker_id))
+        self.conn.commit()
