72ad17928e712efe33dc934f5b03fd4b7cf5c448
MahinAshraful <mahinashraful08@gmail.com>
2024-09-20 23:20:52 -0400

    added login page and UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import AdminPage from './pages/AdminPage.jsx'
-import ManagerPage from './pages/ManagerPage.jsx'
-import EmployeePage from './pages/EmployeePage.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import AdminPage from './pages/AdminPage.jsx';
+import ManagerPage from './pages/ManagerPage.jsx';
+import EmployeePage from './pages/EmployeePage.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
-import Navbar from './components/Navbar.jsx'
-import SubmitForm from './pages/SubmitForm.jsx'
-import Chatbot from './components/Chatbot.jsx'
+import Navbar from './components/Navbar.jsx';
+import SubmitForm from './pages/SubmitForm.jsx';
+import Chatbot from './components/Chatbot.jsx';
+
+function Main() {
+  const location = useLocation();
+  
+  return (
+    <>
+      <Navbar />
+      {/* avoiding chatbot in the "/" */}
+      {location.pathname !== '/' && <Chatbot className="chatbot-container" />}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/manager" element={<ManagerPage />} />
+        <Route path="/employee" element={<EmployeePage />} />
+        <Route path="/submitform" element={<SubmitForm />} />
+      </Routes>
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <Navbar/>
-        <Chatbot className="chatbot-container" />
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/manager" element={<ManagerPage />} />
-          <Route path="/employee" element={<EmployeePage />} />
-          <Route path="/submitform" element={<SubmitForm />} />
-
-        </Routes>
+        <Main /> 
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
