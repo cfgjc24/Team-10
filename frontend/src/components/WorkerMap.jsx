2c80c7ad4c5e7ef72ae50435fa35d4070006b309
MahinAshraful <mahinashraful08@gmail.com>
2024-09-21 08:09:47 -0400

    simple workermap

--- frontend/src/components/WorkerMap.jsx
+++ frontend/src/components/WorkerMap.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
-const locations = [
-  { latitude: 40.7128, longitude: -74.0060, label: "New York" },
-];
+import axios from 'axios';
 
 export default function SimpleMap() {
+
+  const locations = [
+  { latitude: 40.7128, longitude: -74.0060, label: "New York" }
+];
+
+  const fetchLocations = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/User/Locations/all');
+      console.log('Fetched locations:', response.data.locations);  // Log the data
+    } catch (error) {
+      console.error('Error fetching locations:', error);
+    }
+  };
+
   useEffect(() => {
-    // Create the map instance
-    const map = L.map('map').setView([40.7128, -74.0060], 10); 
+    fetchLocations();
+
+    const map = L.map('map').setView([40.7128, -74.0060], 10); // 
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
     }).addTo(map);
 
-    locations.forEach(location => {
+        locations.forEach(location => {
       const marker = L.marker([location.latitude, location.longitude])
         .addTo(map)
         .bindPopup(`<b>${location.label}</b>`);
     });
 
-    // Cleanup when component is unmounted
     return () => {
       map.remove();
     };
