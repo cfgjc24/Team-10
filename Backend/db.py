afc9a7ccec25fe238402e1a0e2d9952f3520f703
Jtawfik04 <johntawfik106@gmail.com>
2024-09-20 20:20:13 -0400

    fixes + testing + appointments

--- Backend/db.py
+++ Backend/db.py
@@ -1,21 +1,14 @@
 import sqlite3
-from schemas import WorkerManager  
-from schemas import ClientManager
-from schemas import AdminManager
-import datetime
-import bcrypt
-import hashlib
-import os
+
+from schemas.worker import WorkerManager  
+from schemas.client import ClientManager
+from schemas.admin import AdminManager
 
 class DatabaseDriver(object):
     def create_tables(self):
         self.worker_manager.create_worker_table()
         self.client_manager.create_client_table()
         self.admin_manager.create_admin_table()
-        self.create_client_table()
-        self.create_boss_table()
-        self.create_location_table()
-        self.create_appointment_table()
 
     def __init__(self):
         self.conn = sqlite3.connect("safety.db")
@@ -24,5 +17,3 @@ class DatabaseDriver(object):
         self.client_manager = ClientManager(self.conn)
         self.admin_manager = AdminManager(self.conn)
         self.create_tables()
-
-
