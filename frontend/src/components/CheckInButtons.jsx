30ae1377880d0b646bce965984b5e75ad1af54b6
MahinAshraful <mahinashraful08@gmail.com>
2024-09-21 09:47:15 -0400

    added form to add to calendar

--- frontend/src/components/CheckInButtons.jsx
+++ frontend/src/components/CheckInButtons.jsx
@@ -1,42 +1,30 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../index.css";
 
 function CheckInButtons({ onClockIn, onClockOut, isClockIn }) {
   const navigate = useNavigate();
 
-  const handleClockIn = () => {
+   const handleClockIn = () => {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
+      navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
-        try {
-          const response = await axios.post('http://localhost:8000/clock-in', {
-            latitude,
-            longitude,
-            user_id: 1
-          });
-          console.log('Clock in successful:', response.data);
-          onClockIn({ id: response.data.id, latitude, longitude });
-        } catch (error) {
-          console.error('Error clocking in:', error);
-        }
+        console.log('Clock in successful:', { latitude, longitude });
+        onClockIn({ latitude, longitude });
       }, (error) => {
         console.error("Error getting location:", error);
       });
     } else {
       console.error("Geolocation is not supported by this browser.");
     }
+
+  
   };
 
-  const handleClockOut = async () => {
-    try {
-      onClockOut();
-      navigate("/submitform");
-    } catch (error) {
-      console.error('Error clocking out:', error);
-    }
+  const handleClockOut = () => {
+    onClockOut();
+    navigate("/submitform");
   };
 
   return (
