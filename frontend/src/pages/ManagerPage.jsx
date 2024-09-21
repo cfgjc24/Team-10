483ddd2bb677f99727273953da568da9545ae3f7
Aya Salama <asalama0204@gmail.com>
2024-09-20 20:25:40 -0400

    Active/Inactive sections for employee list in Manager view

--- frontend/src/pages/ManagerPage.jsx
+++ frontend/src/pages/ManagerPage.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { act } from 'react';
 import EmployeeMap from '../components/EmployeeMap';
 import IdCard from '../components/IdCard';
+import { Heading, VStack } from '@chakra-ui/react'
 
 const ManagerPage = () => {
+
+    // // function to fetch the employees currently in the field
+    // const getActiveEmployees = async () => {
+    //     const res = await fetch("");
+    //     const data = await res.json();
+    //     const activeEmployees = data.data;
+    //     return activeEmployees.filter(employee => employee.status == 'active');
+    // };
+
+    // // function to fetch the employees NOT currently in the field
+    // const getInactiveEmployees = async () => {
+    //     const res = await fetch("");
+    //     const data = await res.json();
+    //     const inactiveEmployees = data.data;
+    //     return inactiveEmployees.filter(employee => employee.status == 'inactive');
+    // };
+
+    // const activeEmployees = getActiveEmployees();
+    // const inactiveEmployees = getInactiveEmployees();
+
   return (
     <div id="page-container">
-        <div className="col left-side">
-            <IdCard></IdCard>
+        <div class="manager-left-side">
+            <VStack>
+                <Heading>
+                    Active
+                </Heading>
+                {/* {activeEmployees.map((activeEmployee) => ( */}
+                    <IdCard />
+                {/* ))} */}
+
+
+                <Heading>
+                    Inactive
+                </Heading>
+
+                {/* {inactiveEmployees.map((inactiveEmployee) => ( */}
+                    <IdCard />
+                {/* ))} */}
+            </VStack>
         </div>
-        <div className="col right-side">
+        <div class="manager-right-side">
             <EmployeeMap></EmployeeMap>
         </div>
     </div>
