341347e6bdc226662531b758968865838744595f
MahinAshraful <mahinashraful08@gmail.com>
2024-09-21 09:49:20 -0400

    signout button works

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import { Box, Button } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button } from '@chakra-ui/react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import lodestarLogo from "../assets/lodestar-logo.png";
 import "../index.css";
 
 function Navbar() {
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+        navigate("/");
+    };
+
     return (
         <header>
             <div className="header">
@@ -15,11 +22,13 @@ function Navbar() {
                     border='2px'
                     borderColor='green.500'
                     margin={'10px'}
-                    >
-                        Signout
+                    onClick={handleSignOut}
+                >
+                    Signout
                 </Button>
             </div>
         </header>
     );
 }
+
 export default Navbar;
