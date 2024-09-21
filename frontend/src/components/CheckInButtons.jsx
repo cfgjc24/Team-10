1454e49c5590f70b21d8567a3982baa4d2e053fe
Nicole Nicole <nicole.martinez@berkeley.edu>
2024-09-20 20:02:11 -0400

    button ui component

--- frontend/src/components/CheckInButtons.jsx
+++ frontend/src/components/CheckInButtons.jsx
@@ -5,8 +5,9 @@ import "../index.css";
 function CheckInButtons() {
     return(
       <div id="buttons">
-        <button type="button" class="btn btn-success">Success</button>
-        <button type="button" class="btn btn-danger">Report Incident</button>
+        <button type="button" class="btn btn-success btn-list-item">Clock In</button>
+        <button type="button" class="btn btn-primary btn-list-item">Clock Out</button>
+        <button type="button" class="btn btn-danger btn-list-item">Report Incident</button>
       </div>
     );
   }
